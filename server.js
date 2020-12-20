18e068caf54d0284269c46967b5de09a0afb4abf
Allison Haver <allisonbhaver@gmail.com>
2020-12-20 15:02:34 -0500

    test

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var PORT = process.env.PORT || 3306;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+
 app.use(express.static("public"));
 
-// Parse request body as JSON
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
+
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
