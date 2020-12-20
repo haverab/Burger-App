e18291e39eeb454425eea103438b8bd358d3212f
Allison Haver <allisonbhaver@gmail.com>
2020-12-20 13:54:07 -0500

    added config folder and connection.js file

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+  
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
+INSERT INTO burgers (burger_name) VALUES ('Portobello Mushroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
